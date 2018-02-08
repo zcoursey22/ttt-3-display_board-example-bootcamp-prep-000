c23c86691e142c7409c02a4843eda0af281a3f6b
Zachary Coursey <zcoursey22@gmail.com>
2018-02-08 05:50:18 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  puts "test"
+  puts ["   |   |   "]
+  puts ["-----------"]
+  puts ["   |   |   "]
+  puts ["-----------"]
+  puts ["   |   |   "]
 end
 
 display_board
